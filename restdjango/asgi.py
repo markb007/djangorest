d99f2a783faa611aacf1b05a46d0a323993e1815
Mark Brown <mark.brown.666@gmail.com>
2022-07-25 09:00:06 +1000

    Rename directories

--- restdjango/asgi.py
+++ restdjango/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for resttutorial project.
+ASGI config for restdjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resttutorial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restdjango.settings')
 
 application = get_asgi_application()
--- restdjango/wsgi.py
+++ restdjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for resttutorial project.
+WSGI config for restdjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resttutorial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restdjango.settings')
 
 application = get_wsgi_application()
